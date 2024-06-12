50da3dfd46cb6725f938c2387168c79071545732
LeonardBagaskara <85619599+LeonardBagaskara@users.noreply.github.com>
2024-06-12 18:17:46 +0700

    Update db.php

--- db.php
+++ db.php
@@ -2,12 +2,14 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
+$dbname = 'uas_kelompok_2';
 
 
-$conn = new mysqli($host, $user, $pass);
+$conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
 }
-echo "Connected Successfully"
 ?>
