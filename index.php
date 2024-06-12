82e4b9c2cee63775c584d43fd248afb777e0289f
LeonardBagaskara <85619599+LeonardBagaskara@users.noreply.github.com>
2024-06-12 10:19:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+<body>
+
+<h2>INVENTORY</h2>
+
+<table style="width:100%">
+  <tr>
+    <th>id</th>
+    <th>nama</th>
+    <th>stok</th>
+    <th>harga</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Pasir Urug Darat</td>
+    <td>105</td>
+    <td>95000</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Plint Keramik Artistik 10 x 10 cm</td>
+    <td>58</td>
+    <td>66500</td>
+  </tr>
+</table>
+
+
+
+</body>
+</html>
+
