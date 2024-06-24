a218b6b92d867ffad62aed40ee89865cebc2147a
LeonardBagaskara <85619599+LeonardBagaskara@users.noreply.github.com>
2024-06-25 03:18:04 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div id="branding">
+                <h1>Inventory</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="landing_page.php">Home</a></li>
+                    <li><a href="index.php">Inventory</a></li>
+                    <li><a href="about.php">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div class="container">
+        <h1>About Us</h1>
+        <p>This website was created by:</p>
+        <ul>
+            <li>Leonardus Rensly Widya Bagaskara    /   20083000007     /       4A</li>
+            <li>Moch Savero Viadi   /   22083000009     /   4A</li>
+        </ul>
+    </div>
+</body>
+</html>
