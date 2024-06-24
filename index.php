055d393f601497e55d00f772d2dbcb2bd9794451
LeonardBagaskara <85619599+LeonardBagaskara@users.noreply.github.com>
2024-06-25 00:03:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,26 +9,28 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Inventory</title>
+    <title>Inventory</title>
     <link rel="stylesheet" href="styles.css">
     <script src="scripts.js" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <header>
         <div class="container">
             <div id="branding">
-                <h1>Aplikasi Inventory</h1>
+                <h1>Inventory</h1>
             </div>
             <nav>
                 <ul>
-                    <li class="current"><a href="index.php">Home</a></li>
+                    <li><a href="landing_page.php">Home</a></li>
+                    <li><a href="About.php">About</a></li>
                 </ul>
             </nav>
         </div>
     </header>
     <div class="container">
         <h1>Product List</h1>
-        <button onclick="window.location.href='add_product.php'">Add New Product</button>
+        <button class="btn btn-add" onclick="window.location.href='add_product.php'">Add New Product</button>
         <table>
             <thead>
                 <tr>
@@ -47,8 +49,12 @@ $result = $conn->query($sql);
                     <td><?php echo $row['stok']; ?></td>
                     <td><?php echo $row['harga']; ?></td>
                     <td>
-                        <a href="update_product.php?id=<?php echo $row['id']; ?>">Update</a>
-                        <a href="delete_product.php?id=<?php echo $row['id']; ?>" class="delete-btn">Delete</a>
+                    <a href="update_product.php?id=<?php echo $row['id']; ?>" class="update-btn">
+                    <i class="fas fa-edit"></i> Update
+                    </a>
+                    <a href="delete_product.php?id=<?php echo $row['id']; ?>" class="delete-btn" onclick="return confirm('Are you sure you want to delete this item?');">
+                    <i class="fas fa-trash-alt"></i> Delete
+                    </a>
                     </td>
                 </tr>
                 <?php endwhile; ?>
